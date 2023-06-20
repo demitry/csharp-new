3689af755416f5d10b739508a76e1d06a6a9685d
demitry <dpoluektov@gmail.com>
2023-06-20 15:55:41 +0300

    Generalized Async Return Types: void/Task/Task<T> + C# 7: + ValueTask/ValueTask<T>

--- CSharp7Demos/GeneralizedAsyncReturnTypes/Program.cs
+++ CSharp7Demos/GeneralizedAsyncReturnTypes/Program.cs
@@ -2,9 +2,33 @@
 {
     internal class Program
     {
+        // new ValueTask type
+        
+        // Prior to C# 7: return void/Task/Task<T>
+
+        public static async Task<long> GetDirSize(string dir)
+        {
+            if (!Directory.EnumerateFileSystemEntries(dir).Any())
+                return 0; // Still imply the creation of the Task even though you might not actually need it
+
+            // Task<long> is return type so it still needs to be instantiated
+
+            return await Task.Run(() => Directory.GetFiles(dir, "*.*", SearchOption.AllDirectories).Sum(f => new FileInfo(f).Length));
+        }
+
+        // C# 7: + ValueTask/ValueTask<T>
+        // Avoiding of creation of fully fledged Task
+        public static async ValueTask<long> GetDirSize2(string dir)
+        {
+            if (!Directory.EnumerateFileSystemEntries(dir).Any())
+                return 0;
+
+            return await Task.Run(() => Directory.GetFiles(dir, "*.*", SearchOption.AllDirectories).Sum(f => new FileInfo(f).Length));
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine(GetDirSize2("c:\temp").Result);
         }
     }
 }

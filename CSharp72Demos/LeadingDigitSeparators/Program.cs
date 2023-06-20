55ad370ff7bb619c18ce6318bbaaca38d699a8ed
demitry <dpoluektov@gmail.com>
2023-06-21 00:58:09 +0300

    C# 7.1: Pattern-Matching with Generics

---
+++ CSharp72Demos/LeadingDigitSeparators/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace LeadingDigitSeparators
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}

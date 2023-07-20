18ebc3c4c172ccf8bb2b007a20c30e1ad06a9515
demitry <dpoluektov@gmail.com>
2023-07-20 21:22:37 +0300

    C# 9: Code Generators: MS Tutorial Project

---
+++ CSharpCodeGenDemo/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace CSharpCodeGenDemo
+{
+    partial class Program
+    {
+        static void Main(string[] args)
+        {
+            HelloFrom("Generated Code");
+
+            Console.WriteLine(new Foo().Bar);
+            // Even if we have Error CS0246	The type or namespace name 'Foo' could not be found
+            // It runs OK
+        }
+
+        static partial void HelloFrom(string name);
+    }
+}
---
+++ Generator/SimpleGenerator.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.CodeAnalysis;
+
+namespace Generator
+{
+    [Generator]
+    public class SimpleGenerator : ISourceGenerator
+
+    {
+        public void Execute(GeneratorExecutionContext context)
+        {
+            var source = @"class Foo { public string Bar = ""bar""; }";
+            context.AddSource("FooGen.cs", source);
+        }
+
+        public void Initialize(GeneratorInitializationContext context)
+        {
+
+        }
+    }
+}
